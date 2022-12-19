013a845edd5521a310f69ad9e6f9bc2b4fa7da00
sachinv26 <vermas7008@gmail.com>
2022-12-19 22:30:07 +0530

    2 questions added

---
+++ activityselection.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+
+bool mycmp(pair<int, int> a, pair<int, int> b)
+{
+    return (a.second < b.second);
+}
+
+int maxActivities(pair<int, int> arr[], int n)
+{
+    sort(arr, arr + n, mycmp);
+    int prev = 0;
+    int res = 1;
+    for (int curr = 1; curr < n; curr++)
+    {
+        if (arr[curr].first >= arr[prev].second)
+        {
+            res++;
+            prev = curr;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    pair<int, int> arr[] = {make_pair(12, 35), make_pair(10, 20), make_pair(20, 30)};
+    int n = 3;
+    cout << maxActivities(arr, n);
+    return 0;
+}
